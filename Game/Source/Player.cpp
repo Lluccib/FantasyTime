bf023819df142e00be731fcd61b3db3aeffebe4f
Lluccib <lluc.cid@estudiantat.upc.edu>
2023-12-20 21:59:08 +0100

    fix

--- Game/Source/Player.cpp
+++ Game/Source/Player.cpp
@@ -63,7 +63,6 @@ bool Player::Update(float dt)
 
 	b2Vec2 currentVelocity = pbody->body->GetLinearVelocity();
 
-	/*currentVelocity.y = 0.5f;*/
 
 
 	if (life, !isWalking, !jump, !dead, !atacking)
@@ -179,18 +178,18 @@ bool Player::Update(float dt)
 
 	}
 
-	//if (app->input->GetKey(SDL_SCANCODE_F2) == KEY_DOWN) //START FROM SECOND LEVEL
-	//{
-	//	dead = false;
-	//	pbody->body->SetTransform({ PIXEL_TO_METERS(32 * 145), PIXEL_TO_METERS(32 * 16) }, 0);
+	if (app->input->GetKey(SDL_SCANCODE_F2) == KEY_DOWN) //START FROM SECOND LEVEL
+	{
+		dead = false;
+		pbody->body->SetTransform({ PIXEL_TO_METERS(32 * 228), PIXEL_TO_METERS(32 * 16) }, 0);
 
-	//	level = 2;
+		level = 2;
 
-	//	app->render->camera.x = 0;
-	//	app->render->camera.y = -190;
+		app->render->camera.x = 0;
+		app->render->camera.y = -190;
 
 
-	//}
+	}
 
 	if (app->input->GetKey(SDL_SCANCODE_F3) == KEY_DOWN)
 	{
@@ -201,7 +200,7 @@ bool Player::Update(float dt)
 		}
 		else
 		{
-			pbody->body->SetTransform({ PIXEL_TO_METERS(32 * 145), PIXEL_TO_METERS(32 * 16) }, 0);
+			pbody->body->SetTransform({ PIXEL_TO_METERS(32 * 228), PIXEL_TO_METERS(32 * 16) }, 0);
 		}
 	}
 
@@ -224,6 +223,7 @@ bool Player::Update(float dt)
 		atacking = false;
 		destroybody = true;
 		currentAnimation->Reset();
+		currentAnimation->loopCount = 0;
 	}
 	else if (currentAnimation == &Atack1left && currentAnimation->HasFinished())
 	{
@@ -231,6 +231,7 @@ bool Player::Update(float dt)
 		atacking = false;
 		destroybody = true;
 		currentAnimation->Reset();
+		currentAnimation->loopCount = 0;
 
 
 	}
@@ -312,9 +313,9 @@ bool Player::Update(float dt)
 
 	}
 
-	//if (app->render->camera.x - position.x - 100 <= -12900) {
-	//	app->render->camera.x = -6333;
-	//}
+	if (app->render->camera.x - position.x - 100 <= -12900) {
+		app->render->camera.x = -6333;
+	}
 	
 	printf("\r %i", app->render->camera.x);
 
@@ -388,10 +389,6 @@ void Player::OnCollision(PhysBody* physA, PhysBody* physB) {
 		}
 		
 		
-	}
-	if (godmode)
-	{
-
 	}
 	
 	
