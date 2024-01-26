d8182a1441e9df10a191037fa3ff007af5222932
Lluccib <lluc.cid@estudiantat.upc.edu>
2024-01-26 17:57:20 +0100

    FIX

--- Game/Source/Scene.cpp
+++ Game/Source/Scene.cpp
@@ -16,6 +16,7 @@
 #include <sstream>
 #include "ModuleFadeToBlack.h"
 #include "SceneIntro.h"
+#include "HUD.h"
 
 #include "Defs.h"
 #include "Log.h"
@@ -94,6 +95,7 @@ bool Scene::Start()
 	app->physics->Enable();
 	app->map->Enable();
 	app->entityManager->Enable();
+	app->hud->Enable();
 	
 	
 
